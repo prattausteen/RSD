04ea457db16888ff007073f54da8fa06956c3a07
Austin Pratt <61068098+prattausteen@users.noreply.github.com>
2020-02-14 15:58:26 -0700

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Simple CSS/JS challenges
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/captaindestroy/pen/GRJoLbq](https://codepen.io/captaindestroy/pen/GRJoLbq).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,27 @@
+/*
+2. Suppose a percent sign % represents the backspace key being pressed. Write a function that transforms a string containing % into a string without any %.
+
+Examples
+
+erase("he%%l%hel%llo") ➞ "hello"
+
+erase("major% spar%%ks") ➞ "majo spks"
+
+erase("si%%%t boy") ➞ "t boy"
+
+erase("%%%%") ➞ ""
+
+Notes
+
+In addition to characters, backspaces can also remove whitespaces.
+
+If the number of percent signs exceeds the previous characters, remove those previous characters entirely (see example #3).
+
+If there only exist backspaces, return an empty string (see example #4).
+*/
+
+function erase(s) {
+  alert('I didnt completely understand the javascript task, but wanted to showcase my strong points. I\'d love to sit down for an interview and discuss my future goals and if they align with rallysportdirect.com');
+}
+
+erase();
